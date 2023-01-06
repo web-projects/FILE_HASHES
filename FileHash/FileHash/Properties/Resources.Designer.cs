ffaeac333e025a176662f30365ba21bc8510f0cf
Jon <jon.bianco@trustcommerce.com>
2023-01-06 10:19:22 -0800

    Refactored code

--- FileHash/FileHash/Properties/Resources.Designer.cs
+++ FileHash/FileHash/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CheckSum.Properties {
+namespace CheckSumGenerator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CheckSum.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CheckSum.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CheckSumGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
